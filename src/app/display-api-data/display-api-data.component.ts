6f6e00ca797f5522a8842da7204634c6383b835b
santhoshkumarcbe <santhoshkumarcbe35@gmail.com>
2024-04-12 15:41:45 +0530

    feature addition

--- src/app/display-api-data/display-api-data.component.ts
+++ src/app/display-api-data/display-api-data.component.ts
@@ -25,12 +25,13 @@ export class DisplayApiDataComponent {
     this.getUsers().subscribe({
       next: data => {
         this.users = data;
-        console.log(this.users);
+        // console.log(this.users);
       },
       error: error => {
         console.error(error);
       }
     })
+
   }
 
   getUsersUrl = `https://jsonplaceholder.typicode.com/users`
@@ -44,18 +45,19 @@ export class DisplayApiDataComponent {
   userForm: FormGroup | null = null;
 
   editUserDetails(user: User): void {
-    console.log(user);
+    // console.log(user);
     this.data = user;
     this.isUserEditFormOpen = true
 
     this.userForm = new FormGroup<UserForm>({
       name: new FormControl(this.data.name, [Validators.required, this.forbiddenNameValidator()]),
-      username: new FormControl(this.data.username, [Validators.required, this.passValueToPhone()]),
+      username: new FormControl(this.data.username, [Validators.required, this.checkUsernameForPhoneValidation()]),
       city: new FormControl(this.data.address.city, [Validators.required]),
-      phone: new FormControl(this.data.phone,[],[this.phoneValidation()]),
+      phone: new FormControl(this.data.phone, [this.phoneNumberRequiredValidation()]),
       website: new FormControl(this.data.website, [Validators.required]),
       company: new FormControl(this.data.company.name, [Validators.required])
     })
+
   }
 
   forbiddenNameValidator(): ValidatorFn {
@@ -66,43 +68,26 @@ export class DisplayApiDataComponent {
     };
   }
 
-  // isPhoneNumberOptional = false;
+  isPhoneNumberOptional = false;
   username = '';
-  passValueToPhone(): ValidatorFn {
-    this.phoneValidation()
+
+  checkUsernameForPhoneValidation(): ValidatorFn {
     const names = ['santhosh', 'jeevan'];
+    // this.userForm?.controls['phone'].markAsDirty();
     return (control: AbstractControl): ValidationErrors | null => {
       this.username = control.value
-      console.log('username', this.username);
-      
       const forbidden = names.some(name => name === control.value);
-      // this.isPhoneNumberOptional = forbidden;
-      // console.log("forbidden", this.isPhoneNumberOptional);
+      this.isPhoneNumberOptional = forbidden;
       return null;
     }
   }
 
-  checkIfUsernameExists(value: string, phonenumber: string) {
-    const names = ['santhosh', 'jeevan'];
-    console.log('value',value , 'phonenumber', phonenumber);
-    
-    return of(names.some((a) =>{ 
-      a !== value && phonenumber === null
-    })).pipe(
-      delay(10)
-    );
+  phoneNumberRequiredValidation(): ValidatorFn {
+    return (control:  AbstractControl): ValidationErrors | null => {
+      const error: boolean = !this.isPhoneNumberOptional && control.value === ''
+      return error ? {required: true} : null;
+    };
   }
-
-  phoneValidation(): AsyncValidatorFn{
-    return (control: AbstractControl): Observable<ValidationErrors | null> => {
-      return this.checkIfUsernameExists(this.username, control.value)
-      .pipe(
-        map((result: boolean) => 
-          result ? { phoneNumberOptional: true } : null
-        )
-      );
-  }
-}
 
   onCancelClick(): void {
     this.isUserEditFormOpen = false;
@@ -110,7 +95,7 @@ export class DisplayApiDataComponent {
 
   onOkClick() {
     if (this.userForm !== null && this.userForm.invalid) {
-      console.log("form not valid");
+      // console.log("form not valid");
     }
     else {
       if (this.data !== null && this.userForm !== null) {
